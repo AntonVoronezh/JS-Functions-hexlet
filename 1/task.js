ae618b3abb1d50cec4303588174469f3687c358d
AntonVoronezh <pstut@mail.ru>
2018-09-21 18:58:23 +0300

    1 #5

--- 1/task.js
+++ 1/task.js
@@ -1,14 +1,14 @@
-const apply = (num, func, arg) => {
-
+const apply = (numarg, func, arg) => {
+  let num = numarg;
   if (num > 0) {
-      while (0 < num) {
-        num -= 1;
-        const sum = apply(num, func, func(arg));
-        return sum;
-      }
+    while (num > 0) {
+      num -= 1;
+      const sum = apply(num, func, func(arg));
+      return sum;
+    }
   } else {
     return arg;
   }
-}
+};
 
 export default apply;
